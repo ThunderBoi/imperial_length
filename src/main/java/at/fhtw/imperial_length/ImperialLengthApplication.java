294d4bb06daffa0fed5ac474336c76d68b465dba
bic4b22_02 <LukasDonner15@gmail.ccom>
2022-12-06 19:41:48 +0100

    Initial commit - project creation

---
+++ src/main/java/at/fhtw/imperial_length/ImperialLengthApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.imperial_length;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialLengthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperialLengthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtw/imperial_length/ImperialLengthApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.imperial_length;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperialLengthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
